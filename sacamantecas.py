29a7f1cb7b2273586ed3b4c5168c7da422a980e6
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2022-03-01 09:45:07 +0100

    Add more comment markers

--- sacamantecas.py
+++ sacamantecas.py
@@ -73,6 +73,20 @@ if sys.platform != 'win32':
 atexit.register(lambda: (print('\nPulse cualquier tecla para continuar...', end='', flush=True), getch()))
 
 
+################################################################
+#                                                              #
+#                                                              #
+#                                               .d88 88b.      #
+#                                              d88P" "Y88b     #
+#                                             d88P     Y88b    #
+#     .d88b.  888d888 888d888 .d88b.  888d888 888       888    #
+#    d8P  Y8b 888P"   888P"  d88""88b 888P"   888       888    #
+#    88888888 888     888    888  888 888     Y88b     d88P    #
+#    Y8b.     888     888    Y88..88P 888      Y88b. .d88P     #
+#     "Y8888  888     888     "Y88P"  888       "Y88 88P"      #
+#                                                              #
+#                                                              #
+################################################################
 def error(message):
     """
     Show the error message 'message' on a Windows API MessageBox and stderr.
@@ -87,6 +101,23 @@ def error(message):
     print(f'\n*** Error en {PROGRAM_NAME}\n{message}', file=sys.stderr, end='')
 
 
+################################################################################################
+#                                                                                              #
+#                                                                                              #
+#                                                888    888                        888         #
+#                                                888    888                        888         #
+#                                                888    888                        888         #
+#     .d88b.  888  888  .d8888b .d88b.  88888b.  888888 88888b.   .d88b.   .d88b.  888  888    #
+#    d8P  Y8b `Y8bd8P' d88P"   d8P  Y8b 888 "88b 888    888 "88b d88""88b d88""88b 888 .88P    #
+#    88888888   X88K   888     88888888 888  888 888    888  888 888  888 888  888 888888K     #
+#    Y8b.     .d8""8b. Y88b.   Y8b.     888 d88P Y88b.  888  888 Y88..88P Y88..88P 888 "88b    #
+#     "Y8888  888  888  "Y8888P "Y8888  88888P"   "Y888 888  888  "Y88P"   "Y88P"  888  888    #
+#                                       888                                                    #
+#                                       888                                                    #
+#                                       888                                                    #
+#                                                                                              #
+#                                                                                              #
+################################################################################################
 # Define the default exception hook.
 def excepthook(exc_type, exc_value, exc_traceback):
     """Handle unhandled exceptions, default exception hook."""
