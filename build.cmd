edea190113a9797a9b7743cb4389a9be79e669c6
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2022-03-03 07:44:30 +0100

    Create ZIP bundle in building system
    Before, the ZIP bundle was created by hand. Now it is created using the
    new building system. To wit, using PowerShell and the Compress-Archive
    cmdlet, which is present in PowerShell 4.0 and above, so it is safe to
    assume it is usable.
    
    An alternative is to use tar in Windows 10 build 17063 or above, but the
    PowerShell method works in more systems, apparently, and even though it
    is slower the performance is more than acceptable.

--- build.cmd
+++ build.cmd
@@ -8,6 +8,10 @@ set VENV_DIR=%VENV_DIR:~0,-1%
 set WORKPATH=%VENV_DIR%\build
 set DISTPATH=%VENV_DIR%\dist
 
+rem Get version code of the current release.
+for /F "tokens=2 delims=^=' " %%a in ('findstr /b "__version__" sacamantecas.py') do set VERSION=%%a
+echo Building sacamantecas %VERSION%
+
 if not exist "%VENV_DIR%\" (
     echo Virtual environment does not exist at "%VENV_DIR%"
     call venv.cmd
@@ -24,6 +28,11 @@ call "%VENV_DIR%\Scripts\deactivate.bat"
 if exist "%DISTPATH%\sacamantecas.exe" (
     echo Copying binary
     copy /b /y "%DISTPATH%\sacamantecas.exe" . >NUL
+    echo Creating ZIP bundle
+    set ZIPCOMMAND=Compress-Archive -Force
+    set ZIPCOMMAND=%ZIPCOMMAND% -Path 'sacamantecas.exe','sacamantecas.ini'
+    set ZIPCOMMAND=%ZIPCOMMAND% -DestinationPath 'sacamantecas %VERSION%.zip'
+    powershell -NoLogo -NonInteractive -NoProfile -Command "$progressPreference='silentlyContinue';%ZIPCOMMAND%"
 )
 
 endlocal
