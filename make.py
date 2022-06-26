32ca3cf59137fd72b890ecf17bec50869bd63f1d
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2022-06-26 17:40:28 +0200

    Refactor code in make.py
    The next step for integrating all helper scripts is to reorganize code
    in make.py so different 'make targets' can be run, not only building the
    frozen executable.
    
    Also, a basic framework to handle the command line so all the different
    targets can be invoked with different arguments on the command line has
    been added.

--- make.py
+++ make.py
@@ -13,25 +13,9 @@ from mkvenv import get_venv_path, is_venv_active, mkvenv, VenvCreationError
 from utils import PROGRAM_ROOT, PROGRAM_PATH, PROGRAM_NAME, PROGRAM_LABEL, error, run, RunError
 
 
-def main():
-    """."""
-    print(f'Building {PROGRAM_LABEL}')
-
-    # Create virtual environment and get its location.
-    if is_venv_active():
-        venv_path = get_venv_path()
-    else:
-        try:
-            print('Creating virtual environment.')
-            venv_path = mkvenv()
-            print(f'Virtual environment created at «{venv_path}».')
-        except VenvCreationError as exc:
-            error(f'creating virtual environment.\n{exc}.')
-            return 1
-
-    # The virtual environment is guaranteed to work from this point on.
-
-    # Build the frozen executable.
+def build_frozen_executable(venv_path):
+    """Build frozen executable."""
+    print(f'Building {PROGRAM_LABEL} frozen executable.')
     pyinstaller_path = venv_path / 'Scripts' / 'pyinstaller.exe'
     build_path = pyinstaller_path.parent.parent / 'build'
     dist_path = build_path.with_stem('dist')
@@ -51,7 +35,7 @@ def main():
             print(exc.stderr)
     if result.returncode or not executable.exists():
         error('Executable was not created.')
-        return 1
+        return False
 
     # Executable was created, so create ZIP bundle.
     bundle_path = PROGRAM_ROOT / f'{PROGRAM_LABEL.replace(" ", "_")}.zip'
@@ -60,8 +44,48 @@ def main():
         inifile = PROGRAM_PATH.with_suffix('.ini')
         bundle.write(executable, executable.name)
         bundle.write(inifile, inifile.name)
-
     print('Successful build.')
+    return True
+
+
+def create_virtual_environment(arg):
+    """Create virtual environment."""
+
+
+def run_unit_tests(arg):
+    """Run the automated unit test suite."""
+
+
+def main():
+    """."""
+    if len(sys.argv):
+        match sys.argv[0].upper():
+            case 'B' | 'BUILD':  # Build executable.
+                operation = build_frozen_executable
+            case 'V' | 'VENV':  # Create virtual environment.
+                operation = create_virtual_environment
+            case 'T' | 'TEST':  # Run the unit tests.
+                operation = run_unit_tests
+            case _:  # By default, build executable.
+                operation = build_frozen_executable
+
+    # Create virtual environment and get its location.
+    if is_venv_active():
+        venv_path = get_venv_path()
+    else:
+        try:
+            print('Creating virtual environment.')
+            venv_path = mkvenv()
+            print(f'Virtual environment created at «{venv_path}».')
+        except VenvCreationError as exc:
+            error(f'creating virtual environment.\n{exc}.')
+            return 1
+
+    # The virtual environment is guaranteed to work from this point on.
+
+    # Build the frozen executable.
+    if not operation(venv_path):
+        return 1
     return 0
 
 
