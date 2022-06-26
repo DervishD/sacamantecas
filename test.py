303db3668abc26d68eba6ece40585b30574ad935
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2022-06-26 16:13:17 +0200

    Rewrite entire test suite
    The automated unit testing system has been rewritten so it is easier to
    expand in the future. Now it consists on a set of classes and no just a
    couple of ad-hoc functions.
    
    The changes are too many to mention here as it is a full rewrite rather
    than a refactor.
    
    Python unittest has not been used because it was cumbersome to use, and
    quite difficult to customize. This code works better for the time being.

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ Test sacamantecas.
 Runs sacamantecas test suite.
 """
 import sys
+import os
 import io
 import re
 from difflib import unified_diff
@@ -13,95 +14,109 @@ from mkvenv import get_venv_path, is_venv_active
 from utils import PROGRAM_LABEL, SCRIPT_NAME, RunError, error, run
 
 
-def run_single_test(command, testitem):
-    """Run a single test from the suite."""
-    if testitem.endswith(('.txt', '.xlsx')):
-        # testitem is a text or Excel file.
-        testitem = outfile = reffile = Path(testitem)
-        testitem = 'tests' / testitem
-    else:
-        # testitem is an URI.
-        outfile = reffile = Path(re.sub(r'\W', '_', f'test_{testitem}', re.ASCII)).with_suffix('.txt')
-    outfile = ('tests' / outfile).with_stem(f'{outfile.stem}_out')
-    reffile = ('tests' / reffile).with_stem(f'{reffile.stem}_ref')
-
-    # Remove output from previous runs (outfile)
-    outfile.unlink(missing_ok=True)
-
-    # Run the test.
-    try:
-        result = run((*command, testitem))
-    except RunError as exc:
-        return (exc.stderr, )
-
-    # If testitem is an URI, the test output must be written to the output file,
-    # since by default is just dumped to console.
-    if not str(testitem).endswith(('.txt', '.xlsx')):
-        with open(outfile, 'w', encoding='utf-8') as dumpfile:
-            dumpfile.write(result.stdout)
-
-    # Compare the resulting files.
-    # If any of them does not exist, consider the test failed.
-    try:
-        if str(outfile).endswith('txt'):
-            # For text files just read them into memory.
-            with open(outfile, encoding='utf-8') as ofile:
-                outlines = ofile.readlines()
-            with open(reffile, encoding='utf-8') as rfile:
-                reflines = rfile.readlines()
-        else:
-            # For Excel files, treat them as Zip files and compare the data
-            # inside. To wit, 'xl/worksheets/sheet1.xml' file.
-            with ZipFile(outfile, 'r') as ofile:
-                with ofile.open('xl/worksheets/sheet1.xml') as sheet:
-                    outlines = io.TextIOWrapper(sheet, encoding='utf-8').readlines()
-            with ZipFile(reffile, 'r') as rfile:
-                with rfile.open('xl/worksheets/sheet1.xml') as sheet:
-                    reflines = io.TextIOWrapper(sheet, encoding='utf-8').readlines()
-    except FileNotFoundError as exc:
-        return (f'*** File {exc.filename} does not exist.\n', )
-
-    # Get diff lines and truncate them if needed.
-    diff = unified_diff(outlines, reflines, str(outfile), str(reffile), n=1)
-    diff = [line.rstrip()[:69] + '…\n' if len(line.rstrip()) > 70 else line for line in diff]
-    if diff:
-        diff.insert(0, '*** Differences found:\n')
-        return diff
-    return ()
+# Command for running the script for the tests.
+COMMAND = (get_venv_path().resolve() / 'Scripts' / 'python.exe', Path(SCRIPT_NAME).resolve())
+# Directory containing the tests.
+TESTS_PATH = Path('tests').resolve()
 
 
-def run_test_suite(command):
-    """Run the automated test suite."""
-    tests = (
-        'file:///./tests/http___ceres_mcu_es_pages_Main_idt_134248_inventary_DE2016_1_24_table_FMUS_museum_MOM.html',
-        'test_local.txt',
-        'test_local.xlsx',
-        'http://ceres.mcu.es/pages/Main?idt=134248&inventary=DE2016/1/24&table=FMUS&museum=MOM',
-        'test_network.txt',
-        'test_network.xlsx',
-    )
+class TestBase():
+    """Base class for all tests."""
+    def __init__(self, testname, testitem):
+        self.testname = testname
+        self.testitem = testitem  # Item under test.
+        self.basename = Path(testitem)  # For computing needed filenames.
+        self.outfile = None  # File produced as output of the test.
+        self.reffile = None  # Reference file to check if the output file is correct or not.
+        self.reason = ()  # Reason of test failure, a tuple of lines.
 
-    for testitem in tests:
-        testname = ''
-        if testitem.endswith('.txt'):
-            testname = 'TXT input, '
-        if testitem.endswith('.xlsx'):
-            testname = 'XLSX input, '
-        if testitem.startswith('file://'):
-            testname = 'file URI'
-        if testitem.startswith('http://'):
-            testname = 'http URI (potentially slow)'
-        if '_local' in testitem:
-            testname += 'file URIs'
-        if '_network' in testitem:
-            testname += 'http URIs (potentially slow)'
-        print(f"Running test '{testname}' ", end='', flush=True)
-        if output := run_single_test(command, testitem):
-            print('❌\n')
-            sys.stdout.writelines(output)
+    def run(self):
+        """
+        Run the test.
+
+        Returns True if the test passed, False if it failed.
+
+        If a test fails, details are in 'self.reason', a tuple of lines.
+        """
+        # Build the needed filenames.
+        self.outfile = self.basename.with_stem(f'{self.basename.stem}_out')
+        self.reffile = self.basename.with_stem(f'{self.basename.stem}_ref')
+
+        # Run the script
+        try:
+            run((*COMMAND, self.testitem))
+        except RunError as exc:
+            self.reason = exc.stderr.lstrip().splitlines(keepends=True)
             return False
-        print('✅')
-    return True
+
+        # Load the filename's contents to be compared.
+        # Usually redefined in derived classes.
+        olines, rlines = self.readfiles()
+
+        # Get diff lines and truncate them if needed.
+        diff = unified_diff(olines, rlines, 'test output', 'reference', n=1)
+        diff = [line.rstrip()[:99] + '…\n' if len(line.rstrip()) > 100 else line for line in diff]
+        if diff:
+            diff.insert(0, 'Differences found:\n')
+            self.reason = diff
+            return False
+
+        # Files are identical so remove output file and return success.
+        self.outfile.unlink(missing_ok=True)
+        return True
+
+    def readfiles(self):
+        """Load output and reference files contents into instance attributes."""
+        with open(self.outfile, encoding='utf-8') as ofile:
+            olines = ofile.readlines()
+        with open(self.reffile, encoding='utf-8') as rfile:
+            rlines = rfile.readlines()
+        return (olines, rlines)
+
+
+class TestUri(TestBase):
+    """Class for single URI tests."""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.basename = Path(re.sub(r'\W', '_', self.testitem, re.ASCII)).with_suffix('.txt')
+
+
+class TestTxt(TestBase):
+    """Class for text input file tests."""
+
+
+class TestXls(TestBase):
+    """Class for xlsx input file tests."""
+    def readfiles(self):
+        # Excel files are really Zip files and for comparing the data inside
+        # only the 'xl/worksheets/sheet1.xml' file has to be read.
+        #
+        # Since usually the XML file will consist in a single line, some crude
+        # formatting is done so the differences shown make a bit of sense in
+        # order to check the output Excel file later. The line is broken at XML
+        # tags and newline characters are added.
+        rep = {r'<': '\n<', r'>': '>\n'}
+        with ZipFile(self.outfile, 'r') as ofile:
+            with ofile.open('xl/worksheets/sheet1.xml') as sheet:
+                olines = io.TextIOWrapper(sheet, encoding='utf-8').read()
+                olines = re.sub(r'|'.join(rep.keys()), lambda m: rep[m.group(0)], olines).splitlines(keepends=True)
+        with ZipFile(self.reffile, 'r') as rfile:
+            with rfile.open('xl/worksheets/sheet1.xml') as sheet:
+                rlines = io.TextIOWrapper(sheet, encoding='utf-8').read()
+                rlines = re.sub(r'|'.join(rep.keys()), lambda m: rep[m.group(0)], rlines).splitlines(keepends=True)
+        return (olines, rlines)
+
+
+TESTS = (
+    # pylint: disable-next=line-too-long
+    TestUri('single file URI', 'file:///./html/http___ceres_mcu_es_pages_Main_idt_134248_inventary_DE2016_1_24_table_FMUS_museum_MOM.html'),  # noqa for pycodestyle.
+    TestTxt('text input with file URIs', 'local.txt'),
+    TestXls('xlsx input with file URIs', 'local.xlsx'),
+    # # pylint: disable-next=line-too-long
+    TestUri('single http URI (potentially slow)', 'http://ceres.mcu.es/pages/Main?idt=134248&inventary=DE2016/1/24&table=FMUS&museum=MOM'),  # noqa for pycodestyle.
+    TestTxt('text input with http URIs (potentially slow)', 'network.txt'),
+    TestXls('xlsx input with http URIs (potentially slow)', 'network.xlsx'),
+)
 
 
 def main():
@@ -112,9 +127,21 @@ def main():
         error('detecting virtual environment.\nVirtual environment is not active.')
         return 1
 
-    run_test_suite((get_venv_path() / 'Scripts' / 'python.exe', SCRIPT_NAME))
+    os.chdir(TESTS_PATH)
 
-    return 0
+    some_test_failed = False
+    try:
+        for test in TESTS:
+            print(f'Testing {test.testname} ', end='', flush=True)
+            if test.run():
+                print('✅')
+            else:
+                some_test_failed = True
+                print('❌')
+                sys.stdout.writelines((f'\n*** {test.reason[0]}',) + tuple(f'  {line}' for line in test.reason[1:]))
+    except KeyboardInterrupt:
+        pass
+    return some_test_failed
 
 
 if __name__ == '__main__':
