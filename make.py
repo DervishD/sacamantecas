7dceb63679a8a00c22ffc4990db20177c408ed3d
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2022-06-26 20:06:57 +0200

    Integrate mkvenv.py into make.py
    The next step into helper scripts integration is to incorporate the code
    of mkvenv.py into make.py.
    
    The mkvenv.py file still exists in the repository because it is used by
    test.py for the time being. It will be deleted in the next iteration of
    integration.

--- make.py
+++ make.py
@@ -8,14 +8,101 @@ requirements inside and then runs the building process.
 """
 import sys
 import os
+import venv
 from zipfile import ZipFile, ZIP_DEFLATED
-from mkvenv import get_venv_path, is_venv_active, mkvenv, VenvCreationError
 from utils import PROGRAM_ROOT, PROGRAM_PATH, PROGRAM_NAME, PROGRAM_LABEL, error, run, RunError
+
+
+def is_venv_active():
+    """
+    Check if virtual environment is active.
+
+    This only checks if the 'VIRTUAL_ENV' environment variable is set or not,
+    which is proof that the virtual environment has been activated, but it does
+    NOT check if the virtual environment is properly set up (e.g. all needed
+    packages are installed or not.)
+
+    """
+    return 'VIRTUAL_ENV' in os.environ
+
+
+def get_venv_path():  # pylint: disable=unused-variable
+    """
+    Get the virtual environment path for this project.
+    IT HAS TO BE THE FIRST LINE IN THE '.gitignore' FILE.
+    IT HAS TO CONTAIN THE STRING 'venv' SOMEWHERE.
+    """
+    venv_path = None
+    gitignore = PROGRAM_ROOT / '.gitignore'
+    message = None
+    try:
+        with open(gitignore, encoding='utf-8') as gitignore:
+            venv_path = gitignore.readline().strip()
+            if 'venv' not in venv_path:
+                venv_path = None
+    except FileNotFoundError:
+        message = '.gitignore does not exist'
+    except PermissionError:
+        message = '.gitignore cannot be read'
+    else:
+        if venv_path is None:
+            message = '.gitignore does not contain a virtual environment path'
+        else:
+            venv_path = PROGRAM_ROOT / venv_path
+            if venv_path.exists() and not venv_path.is_dir():
+                message = 'Virtual environment path exists but it is not a directory'
+
+    if message:
+        error(f'finding virtual environment path.\n{message}.')
+        return None
+
+    return venv_path
+
+
+def create_virtual_environment(venv_path):
+    """Create virtual environment."""
+    print(f'Creating virtual environment at «{venv_path}».')
+    if not venv_path.exists():
+        with open(os.devnull, 'w', encoding='utf-8') as devnull:
+            # Save a reference for the original stdout so it can be restored later.
+            duplicated_stdout_fileno = os.dup(1)
+
+            # Flush buffers, because os.dup2() is not aware of them.
+            sys.stdout.flush()
+            # Point file descriptor 1 to devnull to silent general output.
+            os.dup2(devnull.fileno(), 1)
+
+            # The normal output for the calls below is suppressed.
+            # Error output is not.
+            venv.create(venv_path, with_pip=True, upgrade_deps=True)
+
+            # Flush buffers, because os.dup2() is not aware of them.
+            sys.stdout.flush()
+            # Restore file descriptor 1 to its original output.
+            os.dup2(duplicated_stdout_fileno, 1)
+
+    # The virtual environment does not really need to be activated, because the
+    # commands that will be run will be the ones INSIDE the virtual environment.
+    #
+    # So, the only other thing that is MAYBE needed it setting the 'VIRTUAL_ENV'
+    # environment variable so the launched programs can detect they are really
+    # running inside a virtual environment. Apparently this is not essential,
+    # but it is easy to do and will not do any harm.
+    os.environ['VIRTUAL_ENV'] = str(venv_path.resolve())
+
+    try:
+        run((venv_path / 'Scripts' / 'pip.exe', 'install', '-r', PROGRAM_ROOT / 'requirements.txt'))
+    except RunError as exc:
+        error(f'creating virtual environment.\npip: {exc.stderr}.')
+        return False
+
+    print('Virtual environment created successfully.')
+    return True
 
 
 def build_frozen_executable(venv_path):
     """Build frozen executable."""
-    print(f'Building {PROGRAM_LABEL} frozen executable.')
+    print('Building frozen executable.')
     pyinstaller_path = venv_path / 'Scripts' / 'pyinstaller.exe'
     build_path = pyinstaller_path.parent.parent / 'build'
     dist_path = build_path.with_stem('dist')
@@ -34,7 +121,7 @@ def build_frozen_executable(venv_path):
         if exc.returncode:
             print(exc.stderr)
     if result.returncode or not executable.exists():
-        error('Executable was not created.')
+        error('creating executable.')
         return False
 
     # Executable was created, so create ZIP bundle.
@@ -44,12 +131,8 @@ def build_frozen_executable(venv_path):
         inifile = PROGRAM_PATH.with_suffix('.ini')
         bundle.write(executable, executable.name)
         bundle.write(inifile, inifile.name)
-    print('Successful build.')
+    print('Frozen executable built successfully.')
     return True
-
-
-def create_virtual_environment(arg):
-    """Create virtual environment."""
 
 
 def run_unit_tests(arg):
@@ -58,36 +141,36 @@ def run_unit_tests(arg):
 
 def main():
     """."""
-    if len(sys.argv):
-        match sys.argv[0].upper():
-            case 'B' | 'BUILD':  # Build executable.
-                operation = build_frozen_executable
-            case 'V' | 'VENV':  # Create virtual environment.
-                operation = create_virtual_environment
-            case 'T' | 'TEST':  # Run the unit tests.
-                operation = run_unit_tests
-            case _:  # By default, build executable.
-                operation = build_frozen_executable
+    print(f'Making {PROGRAM_LABEL}\n')
 
-    # Create virtual environment and get its location.
-    if is_venv_active():
-        venv_path = get_venv_path()
-    else:
-        try:
-            print('Creating virtual environment.')
-            venv_path = mkvenv()
-            print(f'Virtual environment created at «{venv_path}».')
-        except VenvCreationError as exc:
-            error(f'creating virtual environment.\n{exc}.')
+    # No matter the operation (the 'make target'), the first thing to check is
+    # if the virtual environment is active. If the virtual environment is not
+    # active it will be activated, if it does not exist it has to be created.
+    if (venv_path := get_venv_path()) is None:
+        return 1
+
+    if not is_venv_active():
+        # Create virtual environment.
+        if not create_virtual_environment(venv_path):
             return 1
 
     # The virtual environment is guaranteed to work from this point on.
 
-    # Build the frozen executable.
-    if not operation(venv_path):
+    # If creating and activating the virtual environment was the only operation
+    # requested, then everything is done.
+    if len(sys.argv) < 2 or sys.argv[1].lower() in ('v', 'venv'):
         return 1
+
+    # A different operation was requested.
+    if len(sys.argv) >= 2 and sys.argv[1].lower() in ('e', 'exe'):
+        # Build the frozen executable.
+        return not build_frozen_executable(venv_path)
+
     return 0
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    try:
+        sys.exit(main())
+    except KeyboardInterrupt:
+        sys.exit(1)
