e59aac11da01ebf332a2bd39d2392c89afabfca6
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2023-09-21 06:52:59 +0200

    Add unit tests for load_profiles()

--- conftest.py
+++ conftest.py
@@ -1,10 +1,11 @@
 """Configuration file for pytest."""
 import os
 import subprocess
+from pathlib import Path
 import pytest
 
 @pytest.fixture
-def unreadable_file(tmp_path):  # pylint: disable=unused-variable
+def unreadable_file(tmp_path: Path) -> Path:  # pylint: disable=unused-variable
     """Create a file which is unreadable by the current user."""
     filename = tmp_path / 'unreadable.ini'
     filename.write_text('')
---
+++ test_load_profiles.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3
+"""Test suite for load_profiles()."""
+from pathlib import Path
+import pytest
+from sacamantecas import MissingProfilesError, ProfilesSyntaxError, load_profiles
+
+
+def test_missing(tmp_path: Path) -> None:  # pylint: disable=unused-variable
+    """Test for missing profiles configuration file."""
+    filename = str(tmp_path / 'non_existent_profiles_file.ini')
+    with pytest.raises(MissingProfilesError):
+        load_profiles(filename)
+
+
+def test_unreadable(unreadable_file: Path) -> None:  # pylint: disable=unused-variable
+    """Test for unreadable profiles configuration file."""
+    with pytest.raises(MissingProfilesError):
+        load_profiles(str(unreadable_file))
+
+
+@pytest.mark.parametrize("text,error", [
+    ('o', 'MissingSectionHeader'),
+    ('[s]\no', 'Parsing'),
+    ('[s]\no = v\no = v', 'DuplicateOption'),
+    ('[s]\no = (', 'BadRegex')
+])
+def test_syntax_errors(tmp_path: Path, text: str, error: str) -> None:  # pylint: disable=unused-variable
+    """Test for syntax errors in profiles configuration file."""
+    filename = tmp_path / 'profiles_syntax_error.ini'
+    filename.write_text(text)
+
+    with pytest.raises(ProfilesSyntaxError) as exc:
+        load_profiles(str(filename))
+    assert exc.value.error == error
+
+    filename.unlink()
+
+@pytest.mark.parametrize("text", ['', '[s]'])
+def test_empty(tmp_path: Path, text: str) -> None:  # pylint: disable=unused-variable
+    """Test for empty profiles configuration file."""
+    filename = tmp_path / 'profiles_empty.ini'
+    filename.write_text(text)
+
+    assert load_profiles(str(filename)) == {}
