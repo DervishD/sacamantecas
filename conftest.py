1a96ba0c809906c7809eea1b3249374a0b5ae697
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2023-10-03 09:25:00 +0200

    Add unit tests for setup_logging()

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 """Configuration file for pytest."""
 import os
 import subprocess
+from collections import namedtuple
 from pathlib import Path
 import pytest
 
@@ -13,3 +14,16 @@ def unreadable_file(tmp_path: Path) -> Path:  # pylint: disable=unused-variable
     yield filename
     subprocess.run(['icacls', str(filename), '/grant', f'{os.environ["USERNAME"]}:R'], check=True)
     filename.unlink()
+
+
+LogPaths = namedtuple('LogPaths', ['log', 'debug'])
+@pytest.fixture()
+def log_paths(tmp_path):  # pylint: disable=unused-variable
+    """Generate temporary filenames for logging files."""
+    logfile_path = tmp_path / 'log.txt'
+    debugfile_path = tmp_path / 'debug.txt'
+
+    yield LogPaths(logfile_path, debugfile_path)
+
+    logfile_path.unlink()
+    debugfile_path.unlink()
---
+++ test_setup_logging.py
@@ -0,0 +1,83 @@
+#! /usr/bin/env python3
+"""Test suite for setup_logging()."""
+import logging
+from collections import namedtuple
+import pytest
+from sacamantecas import setup_logging, error, ERROR_HEADER, warning, WARNING_HEADER
+
+
+def test_logging_files_creation(log_paths):  # pylint: disable=unused-variable
+    """Test that the logging files are created propertly."""
+    assert not log_paths.log.is_file()
+    assert not log_paths.debug.is_file()
+
+    setup_logging(log_paths.log, log_paths.debug)
+    logging.shutdown()
+
+    assert log_paths.log.is_file()
+    assert log_paths.debug.is_file()
+
+# The 'expected_contents' argument is a tuple containing four items:
+#   - The expected contents for the logging file.
+#   - The expected contents for the debugging file.
+#   - The expected contents for stdout.
+#   - The expected contents for stderr.
+TEST_MESSAGE = 'Test message'
+ExpectedContents = namedtuple('ExpectedContents', ['logfile', 'debugfile', 'stdout','stderr'])
+@pytest.mark.parametrize('logfunc, expected_contents', [
+    (logging.debug, ExpectedContents(
+        '',
+        f'[DEBUG] {TEST_MESSAGE}',
+        '',
+        ''
+    )),
+    (logging.info, ExpectedContents(
+        TEST_MESSAGE,
+        f'[INFO] {TEST_MESSAGE}',
+        f'{TEST_MESSAGE}\n',
+        ''
+    )),
+    (logging.warning, ExpectedContents(
+        TEST_MESSAGE,
+        f'[WARNING] {TEST_MESSAGE}',
+        '',
+        f'{TEST_MESSAGE}\n'
+    )),
+    (logging.error, ExpectedContents(
+        TEST_MESSAGE,
+        f'[ERROR] {TEST_MESSAGE}',
+        '',
+        f'{TEST_MESSAGE}\n'
+    )),
+    (warning, ExpectedContents(
+        f'{WARNING_HEADER}{TEST_MESSAGE}',
+        f'[WARNING] {WARNING_HEADER}{TEST_MESSAGE}',
+        '',
+        f'{WARNING_HEADER}{TEST_MESSAGE}\n'
+    )),
+    (error, ExpectedContents(
+        f'{ERROR_HEADER}{TEST_MESSAGE}',
+        '\n'.join(f'[ERROR]{" " if line else ""}{line}' for line in f'{ERROR_HEADER}{TEST_MESSAGE}'.splitlines()),
+        '',
+        f'{ERROR_HEADER}{TEST_MESSAGE}\n'
+    ))
+])
+def test_logging_functions(log_paths, capsys, logfunc, expected_contents):  # pylint: disable=unused-variable
+    """Test all logging functions."""
+    setup_logging(log_paths.log, log_paths.debug)
+    logfunc(TEST_MESSAGE)
+    logging.shutdown()
+
+    log_file_contents = log_paths.log.read_text(encoding='utf-8').splitlines()
+    log_file_contents = [' '.join(line.split(' ')[1:]) for line in log_file_contents]
+    log_file_contents = '\n'.join(log_file_contents)
+    assert log_file_contents == expected_contents.logfile
+
+    debug_file_contents = log_paths.debug.read_text(encoding='utf-8').splitlines()
+    debug_file_contents = [' '.join(line.split(' ')[1:]) for line in debug_file_contents]
+    debug_file_contents = '\n'.join(debug_file_contents)
+    assert debug_file_contents == expected_contents.debugfile
+
+    captured_output = capsys.readouterr()
+    assert captured_output.out == expected_contents.stdout
+    assert captured_output.err == expected_contents.stderr
