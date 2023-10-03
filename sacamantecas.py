DervishD/sacamantecas

commit 0b38c5599c8b1fda11b784a44da2b4fd6180116f

Make main() act upon any list, not only sys.argv
Make main() work with any list as argument instead of having sys.argv as
the hardcoded value. This makes unit test easier and it is more correct
code, too.

sacamantecas.py
@@ -1029,14 +1029,13 @@ def keyboard_interrupt_handler(function):
 
 @loggerize
 @keyboard_interrupt_handler
-def main():
+def main(sources):
     """."""
     logging.info(PROGRAM_BANNER)
 
     atexit.register(wait_for_keypress)
 
-    sys.argv.pop(0)
-    if len(sys.argv) == 0:
+    if len(sources) == 0:
         # The input source should be provided automatically if the program
         # is used as a drag'n'drop target which is, in fact, the intended
         # method of operation.
@@ -1061,7 +1060,7 @@ def main():
         return EXITCODE_FAILURE
 
     logging.info(MESSAGES.SKIMMING_MARKER)
-    for source_type, source_name, sink_name, dumpmode in parse_sources(sys.argv):
+    for source_type, source_name, sink_name, dumpmode in parse_sources(sources):
         if dumpmode:
             logging.debug('La fuente de Manteca «%s» será volcada, no procesada.', source_name)
         print(source_type, source_name, sink_name, dumpmode)
@@ -1091,4 +1090,4 @@ def main():
 
 sys.excepthook = excepthook
 if __name__ == '__main__':
-    sys.exit(main())
+    sys.exit(main(sys.argv.pop(0)))

commit 08dbc791791afaf2b91a1eb408280e69c8bbf570

Fix stupid regression in main() argument handling
The regression was caused by 0b38c5599c8b1fda11b784a44da2b4fd6180116f.

It was a shameful newbie mistake, were list.pop(0) was being used as if
it returned the list with the first element removed and not the removed
element instead.

sacamantecas.py
@@ -1090,4 +1090,4 @@ def main(sources):
 
 sys.excepthook = excepthook
 if __name__ == '__main__':
-    sys.exit(main(sys.argv.pop(0)))
+    sys.exit(main(sys.argv[1:]))