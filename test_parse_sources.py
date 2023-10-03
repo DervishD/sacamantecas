3ace933b86f859b5dbc7b654de13bc0437614a31
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2023-10-03 15:31:32 +0200

    Parametrize unit tests for parse_sources()
    Instead of having separate functions for testing the different kinds of
    sources in normal and 'dump' modes, the tests are now carried out by a
    single parametrized function, which tests the sources in both normal and
    'dump' modes.

--- test_parse_sources.py
+++ test_parse_sources.py
@@ -4,40 +4,20 @@ from pathlib import Path
 import pytest
 from sacamantecas import parse_sources
 
-
-@pytest.mark.parametrize("source, source_name, dumpmode", [
-    ('source', 'source', False),
-    ('dump://source_dump', 'source_dump', True),
+# The 'expected' argument is a tuple containing three items:
+#   - The expected source type detected.
+#   - The expected computed source name.
+#   - The expected computed sink name.
+#
+# The different sources are tested in normal and 'dump' modes.
+@pytest.mark.parametrize('source, expected', [
+    ('source', (None, 'source', None)),
+    ('http://source', ('uri', Path('http://source'), Path('http___source_out.txt'))),
+    ('file://source', ('uri', Path('file://source'), Path('file___source_out.txt'))),
+    ('source.txt', ('txt', Path('source.txt'), Path('source_out.txt'))),
+    ('source.xlsx', ('xls', Path('source.xlsx'), Path('source_out.xlsx')))
 ])
-def test_invalid_source(source, source_name, dumpmode):  # pylint: disable=unused-variable
-    """Test for invalid (unsupported) sources."""
-    assert list(parse_sources([source])) == [(None, source_name, None, dumpmode)]
-
-
-@pytest.mark.parametrize("source, manteca_source, sink_name, dumpmode", [
-    ('http://source', Path('http://source'), Path('http___source_out.txt'), False),
-    ('dump://http://source_dump', Path('http://source_dump'), Path('http___source_dump_out.txt'), True),
-    ('file://source', Path('file://source'), Path('file___source_out.txt'), False),
-    ('dump://file://source_dump', Path('file://source_dump'), Path('file___source_dump_out.txt'), True)
-])
-def test_uri_source(source, manteca_source, sink_name, dumpmode):  # pylint: disable=unused-variable
-    """Test for single URI sources."""
-    assert list(parse_sources([source])) == [('uri', manteca_source, sink_name, dumpmode)]
-
-
-@pytest.mark.parametrize("source, source_name, sink_name, dumpmode", [
-    ('source.txt', Path('source.txt'), Path('source_out.txt'), False),
-    ('dump://source_dump.txt', Path('source_dump.txt'), Path('source_dump_out.txt'), True)
-])
-def test_txt_source(source, source_name, sink_name, dumpmode):  # pylint: disable=unused-variable
-    """Test for text file sources."""
-    assert list(parse_sources([source])) == [('txt', source_name, sink_name, dumpmode)]
-
-
-@pytest.mark.parametrize("source, source_name, sink_name, dumpmode", [
-    ('source.xlsx', Path('source.xlsx'), Path('source_out.xlsx'), False),
-    ('dump://source_dump.xlsx', Path('source_dump.xlsx'), Path('source_dump_out.xlsx'), True)
-])
-def test_xls_source(source, source_name, sink_name, dumpmode):  # pylint: disable=unused-variable
-    """Test for Excel file sources."""
-    assert list(parse_sources([source])) == [('xls', source_name, sink_name, dumpmode)]
+def test_parse_sources(source, expected):  # pylint: disable=unused-variable
+    """Test parsing of Manteca sources from command line."""
+    assert list(parse_sources([source])) == [expected + (False,)]
+    assert list(parse_sources([f'dump://{source}'])) == [expected + (True,)]
