a21ba9003eabcd9deaddb1a2bf6e1588402028b0
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2023-10-17 17:56:42 +0200

    Add preliminary unit tests for handlers
    The tests are by no means complete since for now they only make sure the
    URLs are retrieved correctly, but it makes sense to at least test that.

---
+++ tests/test_handlers.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+"""Test suite for the different handlers of URL sources / metadata sinks."""
+from random import randrange
+from uuid import uuid4
+from openpyxl import Workbook
+from openpyxl.utils.cell import get_column_letter
+import sacamantecas as sm
+
+
+SAMPLE_URLS = [f'http://subdomain{i}.domain.tld' for i in range(10)]
+EXPECTED = [item for url in SAMPLE_URLS for item in (url, None)]
+
+
+def test_single_url_handler():  # pylint: disable=unused-variable
+    """Test single URLs."""
+    result = list(sm.single_url_handler(SAMPLE_URLS[0]))
+    assert result == EXPECTED[0:2]
+
+
+def test_textfile_handler(tmp_path):  # pylint: disable=unused-variable
+    """Test textfile handler."""
+    filename = tmp_path / 'urls.txt'
+    filename.write_text('\n'.join(SAMPLE_URLS), encoding='utf-8')
+    result = list(sm.textfile_handler(filename))
+    assert result == EXPECTED
+
+
+def test_spreadsheet_handler(tmp_path):  # pylint: disable=unused-variable
+    """Test spreadsheet handler."""
+    columns = 10
+    filename = tmp_path / 'urls.xlsx'
+    headings = [f'Heading_{i}' for i in range(columns)]
+
+    workbook = Workbook()
+    sheet = workbook.active
+    sheet.append(headings)
+    for column in range(columns):
+        sheet.column_dimensions[get_column_letter(column + 1)].width = 33
+
+    for url in SAMPLE_URLS:
+        row = [uuid4().hex[:randrange(5, 20)] for _ in range(columns)]
+        row.insert(randrange(len(row)), url)
+        sheet.append(row)
+
+    workbook.save(filename)
+    workbook.close()
+
+    result = list(sm.spreadsheet_handler(filename))
+    assert result == EXPECTED
