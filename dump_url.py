6af7a1c52fb271d9312a4bd4534fd305583e985a
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2024-01-13 22:27:04 +0100

    Add helper script for dumping URLs to HTML files
    Since sacamantecas no longer implements a 'dump mode', a helper script
    has been added with the purpose of downloading the contents of a given
    URL and dumping it to an HTML text file. These files can be used later
    for creating new profiles, testing parsers, etc.

---
+++ dump_url.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+"""Dump contents of provided URL to text file. Crudely."""
+import logging
+import sys
+
+from sacamantecas import retrieve_url, url_to_filename
+
+
+def main(*args):
+    """."""
+    logging.getLogger().setLevel(logging.INFO)
+    for url in args:
+        logging.info('Retrieving %s', url)
+        contents, encoding = retrieve_url(url)
+
+        logging.info('Detected encoding: %s', encoding)
+        contents = contents.decode(encoding)
+
+        output_filename = url_to_filename(url).with_suffix('.html')
+        with open(output_filename, 'wt', encoding=encoding) as output_file:
+            logging.info('Dumping web page to %s', output_filename)
+            output_file.write(contents)
+
+
+if __name__ == '__main__':
+    sys.exit(main(*sys.argv[1:]))
