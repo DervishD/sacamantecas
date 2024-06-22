a86f936a7b061eee1f16ae9f4fa27ccba9f0e11f
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2024-06-22 21:47:01 +0200

    Improve version.py
    Code has been simplified and it is more readable.

--- version.py
+++ version.py
@@ -1,16 +1,16 @@
 """Define sacamantecas version, according to Semantic Versioning 2.0 (https://semver.org/)"""
+# pylint: disable=unused-variable
 import sys
+from time import strftime
 
 
 V_MAJOR = '5'
 V_MINOR = '0'
 V_PATCH = '1'
 V_PRERELEASE = 'alpha'
-V_BUILD = __import__('time').strftime('%Y%m%d-%H%M%S')
+V_BUILD = strftime('%Y%m%d-%H%M%S')
 
-SEMVER = f'{V_MAJOR}.{V_MINOR}.{V_PATCH}'
-SEMVER += f'-{V_PRERELEASE}' if V_PRERELEASE else ''
-SEMVER += f'+{V_BUILD}' if V_PRERELEASE and V_BUILD else ''
+SEMVER = f'{V_MAJOR}.{V_MINOR}.{V_PATCH}' + f'-{V_PRERELEASE}+{V_BUILD}' if V_PRERELEASE else ''
 
 # Development mode is enabled if a prerelease version is running within a virtual environment.
-DEVELOPMENT_MODE = bool(V_PRERELEASE and sys.prefix != sys.base_prefix)  # pylint: disable=unused-variable
+DEVELOPMENT_MODE = bool(V_PRERELEASE and sys.prefix != sys.base_prefix)
