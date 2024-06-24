5f989e49cda65985250f1f2194238503f3c8ffd4
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2024-06-24 19:22:25 +0200

    Comment Semantic Versioning regex in test unit
    The test unit for the correctness of the application version code, which
    has to comply with Semantic Versioning, is done using a regex, which is
    quite long and complex. In order to improve readability and ease future
    maintenance, it has been commented and now is re.VERBOSE.

--- tests/test_version.py
+++ tests/test_version.py
@@ -8,9 +8,14 @@ from version import SEMVER
 # Regular expression obtained directly from https://semver.org/
 # from the 'official' sample at https://regex101.com/r/Ly7O1x/3/
 # but without the named capturing groups, as they are not needed.
-#
-# pylint: disable-next=line-too-long
-SEMVER_REGEX = r"^(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-(?:(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?:[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
+SEMVER_REGEX = r"""^
+    # MAJOR.MINOR.PATCH:
+    (?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)
+    # Dot separated prerelease identifiers:
+    (?:-(?:(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?
+    # Dot separated build identifiers:
+    (?:\+(?:[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?
+$"""
 def test_validate_version_string() -> None:  # pylint: disable=unused-variable
     """Test application version string."""
-    assert re.fullmatch(SEMVER_REGEX, SEMVER, re.ASCII) is not None
+    assert re.fullmatch(SEMVER_REGEX, SEMVER, re.ASCII|re.VERBOSE) is not None
