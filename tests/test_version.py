87851965335267bcd8e55e1ff608036888089ccd
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2024-06-26 15:59:08 +0200

    Fix whitespace in test_version.py

--- tests/test_version.py
+++ tests/test_version.py
@@ -4,7 +4,6 @@ import re
 
 from version import SEMVER
 
-
 # Regular expression obtained directly from https://semver.org/
 # from the 'official' sample at https://regex101.com/r/Ly7O1x/3/
 # but without the named capturing groups, as they are not needed.
