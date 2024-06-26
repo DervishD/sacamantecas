0e77af2b8581d3c8a7bc6000f1c9b24fc1e165c1
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2024-06-26 18:06:48 +0200

    Fix missing parentheses in version.py

--- version.py
+++ version.py
@@ -9,7 +9,7 @@ V_PATCH = '1'
 V_PRERELEASE = 'alpha'
 V_BUILD = strftime('%Y%m%d-%H%M%S')
 
-SEMVER = f'{V_MAJOR}.{V_MINOR}.{V_PATCH}' + f'-{V_PRERELEASE}+{V_BUILD}' if V_PRERELEASE else ''
+SEMVER = f'{V_MAJOR}.{V_MINOR}.{V_PATCH}' + (f'-{V_PRERELEASE}+{V_BUILD}' if V_PRERELEASE else '')
 
 # Development mode is enabled if a prerelease version is running within a virtual environment.
 DEVELOPMENT_MODE = bool(V_PRERELEASE and sys.prefix != sys.base_prefix)
