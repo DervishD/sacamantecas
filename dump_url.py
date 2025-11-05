b1d37196687564aa20971f57f597b5845dcae5ce
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2025-11-05 22:11:30 +0100

    Fix logging in dump_url.py utility

--- dump_url.py
+++ dump_url.py
@@ -8,17 +8,18 @@ from sacamantecas import retrieve_url, url_to_filename
 
 def main(*args: str) -> int:
     """."""
-    logging.getLogger().setLevel(logging.INFO)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
     for url in args:
-        logging.info('Retrieving %s', url)
+        logger.info('Retrieving %s', url)
         contents, encoding = retrieve_url(url)
 
-        logging.info('Detected encoding: %s', encoding)
+        logger.info('Detected encoding: %s', encoding)
         contents = contents.decode(encoding)
 
         output_filename = url_to_filename(url).with_suffix('.html')
         with output_filename.open('wt', encoding=encoding) as output_file:
-            logging.info('Dumping web page to %s', output_filename)
+            logger.info('Dumping web page to %s', output_filename)
             output_file.write(contents)
     return 0
 
