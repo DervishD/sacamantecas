488c874e631f30c25226c3c104487e8c75f4cbc0
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2026-05-09 21:38:58 +0200

    Adapt dump_url.py to latest legion version

--- src/dump_url.py
+++ src/dump_url.py
@@ -1,14 +1,16 @@
 """Dump contents of provided URL to text file. Crudely."""
-import logging
 import sys
+
+from legion import ensure_utf8_output, excepthook, get_logger
 
 from sacamantecas import retrieve_url, url_to_path
 
 
+@ensure_utf8_output
 def main(*args: str) -> int:
     """."""
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
+    logger = get_logger(__name__)
+    logger.config()
     for url in args:
         logger.info('Retrieving %s', url)
         contents, encoding = retrieve_url(url)
@@ -17,11 +19,11 @@ def main(*args: str) -> int:
         contents = contents.decode(encoding)
 
         output_path = url_to_path(url).with_suffix('.html')
-        with output_path.open('wt', encoding=encoding) as output_file:
-            logger.info('Dumping web page to %s', output_path)
-            output_file.write(contents)
+        logger.info('Dumping web page to %s', output_path)
+        output_path.write_text(contents, encoding='utf-8')
     return 0
 
 
 if __name__ == '__main__':
+    sys.excepthook = excepthook
     sys.exit(main(*sys.argv[1:]))
