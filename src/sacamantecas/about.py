725ca16437d0c559554300c901eee9985a2f8018
Raúl Núñez de Arenas Coronado <raulnac@gmail.com>
2026-07-30 09:47:18 +0200

    Update pyproject.toml

--- src/sacamantecas/about.py
+++ src/sacamantecas/about.py
@@ -3,7 +3,7 @@
 # pylint: disable=unused-variable
 PROGRAM_NAME = 'sacamantecas'
 VERSION = '5.4.0.post0'
-BUILD = '5.4.0.post127+development.b01d2eb.dirty'
+BUILD = '5.4.0.post128+development.6bbf074.dirty'
 DEVELOPMENT_MODE = not bool('')
 REPOSITORY = 'https://github.com/DervishD/sacamantecas'
 DEPENDENCIES = ['legion @ git+https://github.com/DervishD/legion.git@v3.0.6', 'openpyxl==3.1.5']
